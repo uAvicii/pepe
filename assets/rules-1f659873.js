c90350f154aabbc9c5a74af6688d6189dbc366a6
Pepe Avicii <1468585714@qq.com>
2024-03-12 17:46:00 +0800

    Updates

---
+++ assets/rules-1f659873.js
@@ -0,0 +1,1 @@
+const e=[{required:!0,message:"请输入手机号"},{pattern:/^1[3-9]\d{9}$/,message:"手机号格式不正确"}],s=[{required:!0,message:"请输入密码"},{pattern:/^\w{8,24}$/,message:"密码需8-24个字符"}];export{e as m,s as p};
